df1d74e04166e000fff4d398b970b0c838971ffc
maxi2003-cloud <maxygamboa659@gmail.com>
2024-11-16 16:06:19 -0300

    actualizacion de front end

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace Restaurante.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Contacto() { 
+        public IActionResult Administrar() { 
             return View();
         }
 
